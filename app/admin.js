a18a2f341b76bebfc03654ff2da097c33f642c3e
ahmed essam <ahmedessamyounsa@example.com>
2024-05-11 01:42:58 +0300

    counter to warn admin

--- app/admin.js
+++ app/admin.js
@@ -10,10 +10,99 @@ import { useFocusEffect } from 'expo-router';
 import { useCallback } from 'react';
 import ContactMessagesPage from './dashboard/manageEmails';
 import OrdersPage from './dashboard/manageOrders';
+import { collection, getDoc, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { db, FIREBASE_AUTH } from './FirebaseConfig';
+import { useEffect } from 'react';
 // Import other components as needed
 
 const AdminPage = () => {
     const [activeTab, setActiveTab] = useState('Products'); // State to track active tab
+
+    const [products, setProducts] = useState(false);
+    const [orders, setOrders] = useState(false);
+    const [contact, setContact] = useState(false);
+    const [notReadyOrdersCount, setNotReadyOrdersCount] = useState(0);
+    const [notRespondedMessagesCount, setNotRespondedMessagesCount] = useState(0);
+    const [outOfStockCount, setOutOfStockCount] = useState(0);
+    const [loading, setLoading] = useState(true);
+
+    function getProducts() {
+        const productsRef = collection(db, "products");
+        const unsubscribe = onSnapshot(productsRef, (snapshot) => {
+            const productsData = [];
+            let outOfStockCount = 0;
+            snapshot.forEach((doc) => {
+                const productData = { id: doc.id, ...doc.data() };
+                if (productData.quantity === 0) {
+                    outOfStockCount++;
+                }
+                productsData.push(productData);
+            });
+            setProducts(productsData);
+            setOutOfStockCount(outOfStockCount);
+            setLoading(false)
+        }, (error) => {
+            console.error("Error getting products from Firestore: ", error);
+        });
+
+        return unsubscribe;
+    }
+
+    useEffect(() => {
+        const unsubscribeOrders = getOrders();
+        const unsubscribeContact = getContact();
+        const unsubscribeProducts = getProducts();
+        return () => {
+            unsubscribeOrders();
+            unsubscribeContact();
+            unsubscribeProducts();
+        };
+    }, []);
+
+    function getOrders() {
+        const ordersRef = collection(db, 'orders');
+        const q = query(ordersRef, orderBy('timestamp', 'desc'));
+
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            const ordersData = querySnapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+            const notReadyOrdersData = ordersData.filter(
+                (order) => order.isReady === 'no'
+            );
+            setNotReadyOrdersCount(notReadyOrdersData.length);
+            setOrders(ordersData);
+        });
+
+        return unsubscribe;
+    }
+
+    function getContact() {
+        const contactMessagesCollectionRef = collection(db, 'contacts');
+        const orderByDate = orderBy('createdAt');
+        const contactMessagesQuery = query(
+            contactMessagesCollectionRef,
+            orderByDate
+        );
+        const unsubscribe = onSnapshot(contactMessagesQuery, (querySnapshot) => {
+            const contactMessagesData = [];
+            querySnapshot.forEach((doc) => {
+                const contactMessageData = doc.data();
+                contactMessageData.id = doc.id;
+                contactMessagesData.push(contactMessageData);
+            });
+            const notRespondedMessagesData = contactMessagesData.filter(
+                (contactMessage) => !contactMessage.responded
+            );
+            setNotRespondedMessagesCount(notRespondedMessagesData.length);
+            setContact(contactMessagesData);
+        });
+
+        return unsubscribe;
+    }
+
+
 
     const handleBackButton = useCallback(() => {
         if (activeTab === 'Products') {
@@ -75,6 +164,9 @@ const AdminPage = () => {
                         color={activeTab === 'Products' ? '#1DA1F2' : '#657786'}
                     />
                     <Text style={styles.buttonText}>Products</Text>
+                    {outOfStockCount > 0 ? <View style={{backgroundColor:'#1DA1F2',width:20,height:20,alignItems:'center',justifyContent:"center",borderRadius:20,position:'absolute',bottom:40,left:27}}>
+                        <Text style={{fontFamily:'lonsfont',color:'white',fontSize:12}}>{outOfStockCount}</Text>
+                    </View> : null}
                 </TouchableOpacity>
                 <TouchableOpacity
                     style={styles.button}
@@ -120,6 +212,9 @@ const AdminPage = () => {
                         color={activeTab === 'Emails' ? '#1DA1F2' : '#657786'}
                     />
                     <Text style={styles.buttonText}>Emails</Text>
+                    {notReadyOrdersCount > 0 ? <View style={{backgroundColor:'#1DA1F2',width:20,height:20,alignItems:'center',justifyContent:"center",borderRadius:20,position:'absolute',bottom:40,left:27}}>
+                        <Text style={{fontFamily:'lonsfont',color:'white',fontSize:12}}>{notRespondedMessagesCount}</Text>
+                    </View> : null}
                 </TouchableOpacity>
                 <TouchableOpacity
                     style={styles.button}
@@ -131,6 +226,9 @@ const AdminPage = () => {
                         color={activeTab === 'Orders' ? '#1DA1F2' : '#657786'}
                     />
                     <Text style={styles.buttonText}>Orders</Text>
+                    {notReadyOrdersCount > 0 ? <View style={{backgroundColor:'#1DA1F2',width:20,height:20,alignItems:'center',justifyContent:"center",borderRadius:20,position:'absolute',bottom:40,left:27}}>
+                        <Text style={{fontFamily:'lonsfont',color:'white',fontSize:12}}>{notReadyOrdersCount}</Text>
+                    </View> : null}
                 </TouchableOpacity>
             </View>
         </View>
