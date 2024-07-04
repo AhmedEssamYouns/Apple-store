82da41ed2d7e58c92b31ea8668ab9acff8578ad5
AhmedEssamYouns <ahmedessamyounsa@gmail.com>
2024-07-04 03:51:03 +0300

    fixed bug

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,7 +11,7 @@ export {
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: 'signIN',
 };
 SplashScreen.preventAutoHideAsync();
 
